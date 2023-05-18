41abbad1624345aa819a8fb15edd531a8f6fe20c
Unicornio <129542080+UnicornioR@users.noreply.github.com>
2023-05-18 13:49:11 -0500

    Hola

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Candidatos CAMPUS</title>
 </head>
 <body>
     <header>
@@ -14,31 +14,44 @@
     </header>
     <main>
         <h2> Datos del Candidato </h2>
-        <form action="registrocandidato.php" method="POST">
+        <form method="POST">
             Nombre: <input type="text" name="nombre"><br>
             Apellidos: <input type="text" name="apellidos"><br>
             Email: <input type="text" name="email"><br>
             Teléfono: <input type="text" name="telefono"><br>
-            Nivel de Estudio: <input type="text" name="ne"><br>
-            <input type="submit" value="Enviar">
+            Nivel de Estudio: <br>
+            Bachiller: <input type="radio" name="1"value="basico" >
+            Técnico: <input type="radio" name="1" value="intermedio">
+            Tecnologo: <input type="radio" name="1" value="avanzado"><br>
+            Tecnologo: <input type="radio" name="1" value="avanzado"><br>
+            Tecnologo: <input type="radio" name="1" value="avanzado"><br>
+
+            
 
             <h2> Conocimientos Previos </h2>
-            Teléfono: <input type="radio" name="lenguaje" value="Phython"><br>
+            Python: <input type="radio" name="1" ><br>
+            CSS: <input type="radio" name="1" ><br>
+            C#: <input type="radio" name="1" ><br>
+            HTML: <input type="radio" name="1"><br>
+            PHP: <input type="radio" name="1" ><br>
+            JS: <input type="radio" name="1" ><br>
+            NODEJS: <input type="radio" name="1" ><br>
+            TypeScript: <input type="radio" name="1"><br>
+            
+            <h2> Nivel de Inglés </h2>
+            Básico: <input type="radio" name="1"value="basico" >
+            Intermedio: <input type="radio" name="1" value="intermedio">
+            Avanzado: <input type="radio" name="1" value="avanzado"><br>
 
+            <input type="submit" value="Enviar">
         </form>
 
         <?php
            $lenguajes =  [ ];
+
         ?>
-
         
-
-        <h1> Nivel de inglés </h1>
-        <button>Básico</button>
-        <button>Intermedio</button>
-        <button>Avanzado</button>
-
-
+        <!--action="registrocandidato.php"-->
     </main>
     
 </body>
